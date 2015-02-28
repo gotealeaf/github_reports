ff98ee39bb8c6e98e3cc654e004156c697e78df8
Jim Benton <jim@autonomousmachine.com>
2015-02-28 13:45:38 -0800

    Prevent github credentials from being saved in VCR cassettes.

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -1,9 +1,16 @@
 require 'vcr'
 require 'webmock'
+require 'netrc'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.allow_http_connections_when_no_cassette = true
+
+  # Prevent GitHub credentials from being saved in the cassette files.
+  netrc = Netrc.read
+  username, password = netrc["api.github.com"]
+  c.filter_sensitive_data('<USER>') { username }
+  c.filter_sensitive_data('<TOKEN>') { password }
 end
