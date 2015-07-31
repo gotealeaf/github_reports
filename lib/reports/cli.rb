7e705488e5c5feb0c42b113fe0f777d2ebd029fd
Kevin Wang <knwang@gmail.com>
2015-07-31 13:44:58 -0400

    After middleware

--- lib/reports/cli.rb
+++ lib/reports/cli.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'thor'
+require 'dotenv'
+Dotenv.load
 
 require 'reports/github_api_client'
 require 'reports/table_printer'
@@ -8,6 +10,35 @@ require 'reports/table_printer'
 module Reports
 
   class CLI < Thor
+
+    desc "user_info USERNAME", "Get information for a user"
+    def user_info(username)
+      puts "Getting info for #{username}..."
+
+      client = GitHubAPIClient.new
+      user = client.user_info(username)
+
+      puts "name: #{user.name}"
+      puts "location: #{user.location}"
+      puts "public repos: #{user.public_repos}"
+    rescue Error => error
+      puts "ERROR #{error.message}"
+      exit 1
+    end
+
+    desc "repositories USERNAME", "Load the repo stats for USERNAME"
+    def repositories(username)
+      puts "Fetching repository statistics for #{username}..."
+
+      client = GitHubAPIClient.new
+      repos = client.public_repos_for_user(username)
+
+      puts "#{username} has #{repos.size} public repos.\n\n"
+      repos.each { |repo| puts "#{repo.name} - #{repo.url}" }
+    rescue Error => error
+      puts "ERROR #{error.message}"
+      exit 1
+    end
 
     desc "console", "Open an RB session with all dependencies loaded and API defined."
     def console
@@ -23,5 +54,4 @@ module Reports
     end
 
   end
-
 end
--- lib/reports/github_api_client.rb
+++ lib/reports/github_api_client.rb
@@ -1,9 +1,55 @@
 require 'faraday'
-require 'json'
+require 'logger'
+require_relative 'middleware/logging'
+require_relative 'middleware/authentication'
+require_relative 'middleware/status_check'
+require_relative 'middleware/json_parsing'
 
 module Reports
 
-  class GitHubAPIClient
-  end
+  class Error < StandardError; end
+  class NonexistentUser < Error; end
+  class RequestFailure < Error; end
+  class AuthenticationFailure < Error; end
 
+  User = Struct.new(:name, :location, :public_repos)
+  Repo = Struct.new(:name, :url)
+
+  class GitHubAPIClient
+    def user_info(username)
+      url = "https://api.github.com/users/#{username}"
+
+      response = connection.get(url)
+
+      if response.status == 404
+        raise NonexistentUser, "'#{username}' does not exist"
+      end
+
+      data = response.body
+      User.new(data["name"], data["location"], data["public_repos"])
+    end
+
+    def public_repos_for_user(username)
+      url = "https://api.github.com/users/#{username}/repos"
+
+      response = connection.get(url)
+
+      if response.status == 404
+        raise NonexistentUser, "'#{username}' does not exist"
+      end
+
+      data = response.body
+      data.map {|repo_data| Repo.new(repo_data["full_name"], repo_data["url"])}
+    end
+
+    def connection
+      @connection ||= Faraday::Connection.new do |builder|
+        builder.use Middleware::JSONParsing
+        builder.use Middleware::StatusCheck
+        builder.use Middleware::Authentication
+        builder.use Middleware::Logging
+        builder.adapter Faraday.default_adapter
+      end
+    end
+  end
 end
